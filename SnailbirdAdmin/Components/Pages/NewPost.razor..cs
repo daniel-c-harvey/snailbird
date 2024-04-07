496fa3334caeae77325029317f9c4a251f3f8e77
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-04-07 19:42:35 -0400

    Initial Admin page with post feed with add new post, before intrument add UX improvements and post delete.

---
+++ SnailbirdAdmin/Components/Pages/NewPost.razor..cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using SnailbirdData.DataAdapters;
+using SnailbirdData.Models;
+
+namespace SnailbirdAdmin.Components.Pages
+{
+    public partial class NewPost
+    {
+        private class InstrumentRow
+        {
+            public bool IsNew { get; }
+            public LiveJamPostInstrument Instrument { get; }
+            public InstrumentRow(LiveJamPostInstrument instrument, bool isNew)
+            {
+                Instrument = instrument;
+                IsNew = isNew;
+            }
+        }
+
+        [Inject]
+        IDataAdapter<LiveJamPost> PostAdapter { get; set; } = default!;
+
+        private LiveJamPost Post { get; } = new LiveJamPost();
+
+        private LiveJamPostInstrument NewInstrument { get; set; } = new LiveJamPostInstrument();
+
+        private List<LiveJamPostInstrument> _instruments = new List<LiveJamPostInstrument>();
+        private IEnumerable<InstrumentRow> Instruments => _instruments.Select(i => new InstrumentRow(i, false)).Append(new InstrumentRow(NewInstrument, true));
+
+        private void AddNewInstrument()
+        {
+            _instruments.Add(NewInstrument);
+            NewInstrument = new LiveJamPostInstrument();
+        }
+
+
+        private void RemoveInstrument(LiveJamPostInstrument instrument)
+        {
+            _instruments.Remove(instrument);
+        }
+
+        private void CommitPost()
+        {
+            Post.Instruments = _instruments;
+            PostAdapter.Insert(Post);
+        }
+    }
+}
---
+++ SnailbirdAdmin/Program.cs
@@ -0,0 +1,68 @@
+using SnailbirdAdmin.Components;
+using DataAccess;
+using SnailbirdData;
+using SnailbirdData.DataAdapters;
+using SnailbirdData.Models;
+using SnailbirdData.Providers;
+
+namespace SnailbirdAdmin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            AddGlobalServices(builder.Services);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+
+        private static void AddGlobalServices(IServiceCollection col)
+        {
+            var dataResources = new DataResources<MongoDB.Driver.IMongoDatabase, MongoDataAccess, MongoQueryBuilder>
+            (
+                new MongoDataAccess(
+                    Core.ConnectionStringTools.LoadFromFile("./.secrets/connections.json", "mongodb-snailbird")
+                        .ConnectionString,
+                        "snailbird-dev"
+                    ),
+                new MongoQueryBuilder()
+            );
+
+            MongoAdapter<LiveJamPost> postAdapter = new MongoAdapter<LiveJamPost>(dataResources.DataAccess, dataResources.QueryBuilder, new DataSchema("posts"));
+
+            col
+            .AddSingleton<IDataAdapter<LiveJamPost>, MongoAdapter<LiveJamPost>>
+            (provider =>
+            {
+                return postAdapter;
+            })
+            .AddSingleton<IPostProvider<LiveJamPost>, PostMongoProvider>
+            (provider => new PostMongoProvider(postAdapter));
+
+        }
+    }
+}
