d9c173c4051198874035bc3123107ac022541bed
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-04-08 05:54:01 -0400

    cleaning new post code and UX

--- SnailbirdAdmin/Components/Pages/NewPost.razor..cs
+++ SnailbirdAdmin/Components/Pages/NewPost.razor..cs
@@ -6,31 +6,18 @@ namespace SnailbirdAdmin.Components.Pages
 {
     public partial class NewPost
     {
-        private class InstrumentRow
-        {
-            public bool IsNew { get; }
-            public LiveJamPostInstrument Instrument { get; }
-            public InstrumentRow(LiveJamPostInstrument instrument, bool isNew)
-            {
-                Instrument = instrument;
-                IsNew = isNew;
-            }
-        }
 
         [Inject]
         IDataAdapter<LiveJamPost> PostAdapter { get; set; } = default!;
 
         private LiveJamPost Post { get; } = new LiveJamPost();
 
-        private LiveJamPostInstrument NewInstrument { get; set; } = new LiveJamPostInstrument();
-
         private List<LiveJamPostInstrument> _instruments = new List<LiveJamPostInstrument>();
-        private IEnumerable<InstrumentRow> Instruments => _instruments.Select(i => new InstrumentRow(i, false)).Append(new InstrumentRow(NewInstrument, true));
+        private IEnumerable<LiveJamPostInstrument> Instruments => _instruments;
 
         private void AddNewInstrument()
         {
-            _instruments.Add(NewInstrument);
-            NewInstrument = new LiveJamPostInstrument();
+            _instruments.Add(new LiveJamPostInstrument());
         }
 
 
