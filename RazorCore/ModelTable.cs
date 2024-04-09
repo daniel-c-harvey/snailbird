71bc4717477eb192fab8186501472467a871f0f6
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-04-09 09:23:11 -0400

    Adding Razor Core and New Row Table databindable component, WIP

---
+++ RazorCore/ModelTable.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Core;
+
+namespace RazorCore
+{
+    public class ModelTable<TModel>
+    {
+        public IEnumerable<TModel> Models { get; }
+        private Dictionary<string, ModelColumn<TModel>> _columnMap;
+        public IEnumerable<string> Captions => _columnMap.Keys;
+        public IEnumerable<ModelColumn<TModel>> Columns => _columnMap.Values;
+
+        public ModelTable(IEnumerable<TModel> models, Dictionary<string, ModelColumn<TModel>> columnMap)
+        {
+            Models = models;
+            _columnMap = columnMap;
+        }
+
+
+    }
+}
