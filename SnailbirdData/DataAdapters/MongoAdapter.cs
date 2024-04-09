3aa8f50130ca063dd1ccbb53eebf5ca6e01b41d6
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-04-09 03:13:11 -0400

    Connection Manager page on admin site, no edit yet, only add and delete

--- SnailbirdData/DataAdapters/MongoAdapter.cs
+++ SnailbirdData/DataAdapters/MongoAdapter.cs
@@ -1,11 +1,5 @@
 ﻿using DataAccess;
 using MongoDB.Driver;
-using SnailbirdData.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnailbirdData.DataAdapters
 {
