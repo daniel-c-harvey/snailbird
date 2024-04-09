4deb08f13c1d91dbff1d95413dcba71e0fa0fbbb
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-04-09 15:24:04 -0400

    Table Test improvements, need to debug the reordering

--- RazorCore/ModelColumn.cs
+++ RazorCore/ModelColumn.cs
@@ -6,12 +6,13 @@ namespace RazorCore
     public class ModelColumn<TModel>
     {
         public Func<TModel, object> Binding { get; }
-        public bool IsHeader { get; }
 
-        public ModelColumn(Func<TModel, object> binding, bool isHeader = false)
+        public bool Editable { get; }
+
+        public ModelColumn(Func<TModel, object> binding, bool editable = false)
         {
             Binding = binding;
-            IsHeader = isHeader;
+            Editable = editable;
         }
     }
 }
