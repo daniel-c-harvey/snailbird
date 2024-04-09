47991e6fecd7c51047dfa9cec67ec4ab1a8dc895
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-04-09 09:23:11 -0400

    Adding Razor Core and New Row Table databindable component, WIP

---
+++ RazorCore/ModelColumn.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Core;
+
+namespace RazorCore
+{
+    public class ModelColumn<TModel>
+    {
+        public Func<TModel, object> Binding { get; }
+        public bool IsHeader { get; }
+
+        public ModelColumn(Func<TModel, object> binding, bool isHeader = false)
+        {
+            Binding = binding;
+            IsHeader = isHeader;
+        }
+    }
+}
