f4dd89346c5d0f63d360a364790d48fbda489177
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-04-21 05:27:34 -0400

    Settling on string for columns as that is the databinding base type.

--- RazorCore/ColumnMap.cs
+++ RazorCore/ColumnMap.cs
@@ -4,17 +4,17 @@ namespace RazorCore
     public class ColumnMap<TModel> : IColumnMap<TModel>
     {
         protected IList<string> captions;
-        protected IList<ModelColumnBase<TModel>> columns;
+        protected IList<IModelColumn<TModel>> columns;
 
         public IEnumerable<string> Captions => captions;
-        public IEnumerable<ModelColumnBase<TModel>> Columns => columns;
+        public IEnumerable<IModelColumn<TModel>> Columns => columns;
         public ColumnMap()
         {
             captions = new List<string>();
-            columns = new List<ModelColumnBase<TModel>>();
+            columns = new List<IModelColumn<TModel>>();
         }
 
-        public ColumnMap<TModel> AddColumn(string caption, ModelColumnBase<TModel> column)
+        public IColumnMap<TModel> AddColumn(string caption, IModelColumn<TModel> column)
         {
             captions.Add(caption);
             columns.Add(column);
