5ae68fbdde2e72ec6f7542400d0a94bb05910d9b
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-05-04 06:19:47 -0400

    Adding Choice Features to NewRowTable

--- RazorCore/IModelColumn.cs
+++ RazorCore/IModelColumn.cs
@@ -1,9 +1,12 @@
 ﻿
+using Core;
+
 namespace RazorCore
 {
     public interface IModelColumn<TModel>
     {
         bool Editable { get; }
+        IEnumerable<string> Choices { get; }
         Func<TModel, string> Getter { get; }
         Action<TModel, string> Setter { get; }
     }
