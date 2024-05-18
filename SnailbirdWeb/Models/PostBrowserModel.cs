3e99aab9aefe062b0a45a1b5f5f8afdeb76e5e98
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-05-18 12:57:43 -0400

    Post browser MVU implementation for main site

---
+++ SnailbirdWeb/Models/PostBrowserModel.cs
@@ -0,0 +1,49 @@
+﻿using Core;
+using RazorCore.Navigation;
+using SnailbirdData.Models;
+
+namespace SnailbirdWeb.Models
+{
+    public enum PostBrowserMode
+    {
+        Feed,
+        ViewPost
+    }
+
+    public class PostBrowserFeedModel
+    {
+        public Page Page { get; set; }
+        public IEnumerable<LiveJamPost> Posts { get; set; }
+
+        public PostBrowserFeedModel(Page page, IEnumerable<LiveJamPost> posts)
+        {
+            Page = page;
+            Posts = posts;
+        }
+    }
+
+    public class PostBrowserViewPostModel
+    {
+        public LiveJamPost Post { get; set; }
+
+        public PostBrowserViewPostModel(LiveJamPost post)
+        {
+            Post = post;
+        }
+    }
+
+    public class PostBrowserModel : IMode<PostBrowserMode>
+    {
+        public PostBrowserMode CurrentMode { get; set; }
+        
+        public PostBrowserViewPostModel ViewPost { get; set; }
+        public PostBrowserFeedModel Feed { get; set; }
+        
+        public PostBrowserModel(PostBrowserMode currentMode)
+        {
+            CurrentMode = currentMode;
+            ViewPost = new PostBrowserViewPostModel(new LiveJamPost());
+            Feed = new(new Page(0, 0), new List<LiveJamPost>());
+        }
+    }
+}
---
+++ SnailbirdWeb/Views/PostBrowser.razor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Components;
+using Core;
+using RazorCore.Navigation;
+using SnailbirdData.Models;
+using SnailbirdData.DataAdapters;
+using SnailbirdWeb.Models;
+using SnailbirdWeb.Updates;
+using SnailbirdWeb.Messages;
+
+namespace SnailbirdWeb.Views
+{
+    public partial class PostBrowser : INavigable<PostBrowserMode>
+    {
+        #region "Members"
+        [Inject]
+        public IDataAdapter<LiveJamPost> PostAdapter { get; set; }
+
+        private PostBrowserModel? model;
+        private PostBrowserUpdate? update;
+        private void InitModel()
+        {
+            model = new(PostBrowserMode.Feed);
+            update = new(PostAdapter);
+            update.Update(model, new PostBrowserGetFeedMessage(new Page(0, 25)));
+        }
+        #endregion
+
+        #region "Event Handlers"
+        protected override void OnInitialized()
+        {
+            InitModel();
+            InitNavigation();
+        }
+        #endregion
+
+        #region "View Model"
+        private void ViewPost(LiveJamPost post)
+        {
+            if (update != null && model != null) 
+            {
+                BeforeModeChange();
+                update.Update(model, new PostBrowserViewPostMessage(post));
+            }
+        }
+        #endregion
+
+        #region "INavigable"
+        private void InitNavigation()
+        {
+            Navigator = new Navigator<PostBrowserMode, PostBrowserModel>(model);
+            Navigator.ModeChanging += (_) => StateHasChanged();
+        }
+
+        public INavigator<PostBrowserMode> Navigator { get; private set; }
+        private PostBrowserMode CurrentMode => model.CurrentMode;
+        private void BeforeModeChange()
+        {
+            if (model != null)
+            {
+                Navigator.OnForward();
+            }
+        }
+
+        public void OnBack(PostBrowserMode mode)
+        {
+            if (model != null)
+            {
+                model.CurrentMode = mode;
+                StateHasChanged();
+            }
+        }
+        #endregion
+    }
+}
