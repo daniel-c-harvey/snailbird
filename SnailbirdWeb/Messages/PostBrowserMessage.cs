0e873ddcee94c314a9c6639bbd135875ae48fef8
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-05-18 12:57:43 -0400

    Post browser MVU implementation for main site

---
+++ SnailbirdWeb/Messages/PostBrowserMessage.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using SnailbirdAdmin.Messages;
+using SnailbirdData.Models;
+
+namespace SnailbirdWeb.Messages
+{
+    public enum PostBrowserAction
+    {
+        GetFeed,
+        ViewPost
+    }
+
+    public abstract class PostBrowserMessage : MessageBase<PostBrowserAction>
+    {
+        protected PostBrowserMessage(PostBrowserAction action) : base(action) { }
+    }
+
+    public class PostBrowserGetFeedMessage : PostBrowserMessage
+    {
+        public Page Page { get; }
+
+        public PostBrowserGetFeedMessage(Page page)
+            : base(PostBrowserAction.GetFeed) 
+        {
+            Page = page;
+        }
+    }
+
+    public class PostBrowserViewPostMessage : PostBrowserMessage
+    {
+        public LiveJamPost Post { get; }
+
+        public PostBrowserViewPostMessage(LiveJamPost post)
+            : base(PostBrowserAction.ViewPost)
+        {
+            Post = post;
+        }
+    }
+}
---
+++ SnailbirdWeb/Updates/PostBrowserUpdate.cs
@@ -0,0 +1,46 @@
+﻿using SnailbirdData.Models;
+using SnailbirdData.Providers;
+using SnailbirdWeb.Models;
+using SnailbirdWeb.Messages;
+using SnailbirdData.DataAdapters;
+
+namespace SnailbirdWeb.Updates
+{
+    public class PostBrowserUpdate
+    {
+        private IDataAdapter<LiveJamPost> PostAdapter { get; }
+
+        public PostBrowserUpdate(IDataAdapter<LiveJamPost> postAdapter)
+        {
+            PostAdapter = postAdapter;
+        }
+
+        public PostBrowserModel Update(PostBrowserModel model, PostBrowserMessage message)
+        {
+            switch (message.Action)
+            {
+                case PostBrowserAction.GetFeed:
+                    var getFeedMessage = message as PostBrowserGetFeedMessage;
+                    if (getFeedMessage != null) { GetFeed(model, getFeedMessage); }
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return model;
+        }
+
+        private void GetFeed(PostBrowserModel model, PostBrowserGetFeedMessage message) 
+        {
+            if (PostAdapter != null)
+            {
+                var results = PostAdapter.GetPage(message.Page.PageIndex, message.Page.PageLength);
+                if (results.Success)
+                {
+                    model.Feed.Posts = results.Value;
+                    model.Feed.Page = message.Page;
+                }
+            }
+        }
+    }
+}
