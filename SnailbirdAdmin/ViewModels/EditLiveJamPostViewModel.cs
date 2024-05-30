df45ff55b929c412ca2067d49622b99833dd3d51
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-05-30 09:54:10 -0400

    refactor start to implement inherited view models and composable views, as razor seems to work better with content composition.  WIP

---
+++ SnailbirdAdmin/ViewModels/EditLiveJamPostViewModel.cs
@@ -0,0 +1,44 @@
+﻿using RazorCore;
+using SnailbirdData.Models.Post;
+
+namespace SnailbirdAdmin.ViewModels
+{
+    public class EditLiveJamPostViewModel : EditPostViewModel<LiveJamPost>
+    {
+        private List<Instrument> _instruments = new List<Instrument>();
+        public IEnumerable<Instrument> Instruments => _instruments;
+
+        public static IColumnMap<Instrument> InstrumentColumns = new ColumnMap<Instrument>()
+            .AddColumn("Name",
+                new ModelColumn<Instrument>(
+                    inst => inst.Name,
+                    (inst, name) => inst.Name = name)
+                .MakeEditable())
+            .AddColumn("Description",
+                new ModelColumn<Instrument>(
+                    inst => inst.Description,
+                    (inst, desc) => inst.Description = desc)
+                .MakeEditable());
+
+        public void AddNewInstrument(Instrument instrument)
+        {
+            _instruments.Add(instrument);
+        }
+
+
+        public void RemoveInstrument(Instrument instrument)
+        {
+            _instruments.Remove(instrument);
+        }
+
+        public override void CommitPost()
+        {
+            if (Post != null)
+            {
+                Post.Instruments = _instruments;
+                base.CommitPost();
+            }
+        }
+    }
+}
+
---
+++ SnailbirdAdmin/Views/LiveJamPostManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using SnailbirdData.Models.Post;
+
+namespace SnailbirdAdmin.Views
+{
+    public class LiveJamPostManager : PostManagerBase<LiveJamPost, EditLiveJamPost>
+    {
+        protected override RenderFragment AddComponent => builder =>
+        {
+            builder.OpenComponent(0, typeof(EditLiveJamPost));
+            builder.AddAttribute(1, nameof(EditLiveJamPost.Post), model?.Post);
+            builder.AddAttribute(2, nameof(EditLiveJamPost.OnCommitPost), SaveNewPost);
+            builder.CloseComponent();
+        };
+
+        protected override RenderFragment EditComponent => builder =>
+        {
+            builder.OpenComponent(0, typeof(EditLiveJamPost));
+            builder.AddAttribute(1, nameof(EditLiveJamPost.Post), model?.Post);
+            builder.AddAttribute(2, nameof(EditLiveJamPost.OnCommitPost), SavePost);
+            builder.CloseComponent();
+        };
+    }
+}
