8b431299597ded13bdba3be2782ea3f3fc8fcb86
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-08-07 07:31:28 -0400

    snailbird admin WIP

---
+++ SnailbirdAdmin/ViewModels/FlexElementViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SnailbirdData.Models.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnailbirdAdmin.ViewModels
+{
+    public class FlexElementViewModel
+    {
+        private static Dictionary<string, Type> ElementChoiceMap = new Dictionary<string, Type>
+        {
+            { nameof(PostParagraph), typeof(PostParagraph) },
+            { nameof(PostImage), typeof(PostImage) },
+            { nameof(PostYouTubeEmbed), typeof(PostYouTubeEmbed) },
+            { nameof(PostInstrumentList), typeof(PostInstrumentList) }
+        };
+
+        public PostElement? Element { get; set; }
+    }
+}
