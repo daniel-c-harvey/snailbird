485a81b0ea2c5338a145206f127fbf8c83d56552
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-09-15 16:23:39 -0400

    beginning Expandable component implementation to use in PostFeed, so that the scrollable content is minimal when first loaded.  button action not yet implemented

---
+++ RazorCore/Expandable.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorCore
+{
+    public partial class Expandable
+    {
+        [Parameter]
+        [EditorRequired]
+        public RenderFragment ChildContent { get; set; } = default!;
+
+        private static readonly Dictionary<bool, string> ExpandedStyleMap = new Dictionary<bool, string>() 
+        { 
+            { false, "rc-unexpanded" },
+            { true,  "rc-expanded" }
+        };
+
+        [Parameter]
+        public bool Expanded { get; set; } = false;
+
+        private string Style => ExpandedStyleMap[Expanded];
+
+        private bool ToggleExpansion() => Expanded = !Expanded;
+    }
+}
