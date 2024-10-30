390284476e6623a6ce6ef0796f7913a7edcba66d
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-10-30 12:57:46 -0400

    Implementation for FlexElement type binding / switching element type

--- SnailbirdData/Models/Post/FlexElement.cs
+++ SnailbirdData/Models/Post/FlexElement.cs
@@ -1,34 +1,59 @@
 ﻿
+using NetBlocks;
+
 namespace SnailbirdData.Models.Post
 {
-    public abstract class FlexElement
+    public abstract class FlexElement : IClonable<FlexElement>
     {
         public int Ordinal { get; set; }
         public abstract string TypeCaption { get; }
+
+        public abstract FlexElement Clone();
     }
 
     public class FlexParagraph : FlexElement
     {
         public override string TypeCaption => "Paragraph";
-        public string Text { get; set; }
+        public string Text { get; set; } = string.Empty;
+
+        public override FlexElement Clone()
+        {
+            return new FlexParagraph() { Text = Text };
+        }
     }
 
     public class FlexImage : FlexElement
     {
         public override string TypeCaption => "Image";
-        public string ImageURI { get; set; }
-        public string AltText { get; set; }
+        public string ImageURI { get; set; } = string.Empty;
+        public string AltText { get; set; } = string.Empty;
+
+        public override FlexElement Clone()
+        {
+            return new FlexImage() { ImageURI = ImageURI, 
+                                     AltText = AltText };
+        }
     }
 
     public class FlexYouTubeEmbed : FlexElement
     {
         public override string TypeCaption => "YouTube Embed";
-        public string VideosURL { get; set; }
+        public string VideosURL { get; set; } = string.Empty;
+
+        public override FlexElement Clone()
+        {
+            return new FlexYouTubeEmbed() { VideosURL = VideosURL };
+        }
     }
 
     public class FlexInstrumentList : FlexElement
     {
         public override string TypeCaption => "Instrument List";
-        public IList<Instrument> Instruments { get; set; }
+        public IList<Instrument> Instruments { get; set; } = [];
+
+        public override FlexElement Clone()
+        {
+            return new FlexInstrumentList() { Instruments = Instruments };
+        }
     }
 }
