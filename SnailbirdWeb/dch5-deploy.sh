8c19335cc68b94d83264a656284bf17fcf666627
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-11-18 15:55:10 -0500

    Updated Deployment

--- SnailbirdWeb/dch5-deploy.sh
+++ SnailbirdWeb/dch5-deploy.sh
@@ -1,1 +1,1 @@
-scp -i /c/.ssh/dch5root_rsa -r ./bin/Release/net8.0/publish/* root@dch5.snailbird.net:/root/blazor/bin/snailbird/
+scp -i /c/.ssh/dch5snailbirdRSA -r ./bin/Release/net8.0/publish/* snailbird@dch5.snailbird.net:/snailbird/web/
