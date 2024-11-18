5efbcb9d85bb40e8f52d0c24fdf2cabf7892a772
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-11-18 15:55:00 -0500

    Adding POCO for Secrets, loaded via JSONConfiguration.
    Try to load secrets.json and deserialize into ConnectionSecrets object

---
+++ SnailbirdWeb/Models/ConnectionSecrets.cs
@@ -0,0 +1,14 @@
+﻿namespace SnailbirdWeb.Models
+{
+    public class ConnectionSecrets
+    {
+        public string ActiveConnection { get; set; } = default!;
+        public IEnumerable<Connection> Connections { get; set; } = default!;
+    }
+
+    public class Connection
+    {
+        public string Name { get; set; } = default!;
+        public string ConnectionString { get; set; } = default!;
+    }
+}
